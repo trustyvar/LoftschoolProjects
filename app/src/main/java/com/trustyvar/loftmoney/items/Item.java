22586ec123262db7cd57b8a94ae5b2cf71c83130
trustyvar <trustyvar@gmail.com>
2022-02-11 20:30:16 +0300

    Выполнено ДЗ второй недели

---
+++ app/src/main/java/com/trustyvar/loftmoney/items/Item.java
@@ -0,0 +1,21 @@
+package com.trustyvar.loftmoney.items;
+
+public class Item {
+
+    private final String name;
+    private final String price;
+
+    public Item(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+}
